d8db133ea264b50b687d27f86010efc5f2bfb39c
Faa-ux <67672384+Faa-ux@users.noreply.github.com>
2020-08-22 22:38:27 -0400

    Added weather data
    added windspeed, humidity and weather description to search results

--- weather.js
+++ weather.js
@@ -25,20 +25,31 @@ function search(event) {
   console.log(searchInput.value);
   let h4 = document.querySelector("h4");
   h4.innerHTML = `${searchInput.value}`;
+  let description = document.querySelector("#weatherdescriptor");
+  let speed = document.querySelector("#wind");
+  let humid = document.querySelector("#precipitation");
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=4a3d357424bef28614271bbf95c59986&&units=metric`;
   axios
     .get(apiUrl)
     .then(function (response) {
       console.log(response);
       console.log(response.data.main.temp);
+      console.log(response.data.weather[0].description);
+      console.log(response.data.wind.speed);
+      console.log(response.data.main.humidity);
 
       span.innerHTML = `${days[day]} ${hr}:${min} Temperature:${Math.round(
         response.data.main.temp
       )}°C`;
+      description.innerHTML = response.data.weather[0].description;
+      speed.innerHTML = `${response.data.wind.speed} Km/h`;
+      humid.innerHTML = `${response.data.main.humidity} %`;
     })
+
     .catch((error) => {
       console.log(error);
     });
 }
+
 let form = document.querySelector("#searchcity");
 form.addEventListener("submit", search);
