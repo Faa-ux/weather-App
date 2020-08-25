2c39ab82a3410f5c94514bef64d2c1fac10d9075
Faa-ux <67672384+Faa-ux@users.noreply.github.com>
2020-08-24 23:11:37 -0400

    Added time and corrected html
    added time of weather update and corrected html by reducing amount of columns in use

--- weather.js
+++ weather.js
@@ -1,7 +1,9 @@
-let now = new Date();
-console.log(now.getDate());
-console.log(now.getHours());
-console.log(now.getMinutes());
+//temperature conversion
+//converting seconds to time
+// how do i make the function display the time of the place im searching
+//this gives me my time. thats not what i need
+let date = new Date();
+let day = date.getDay();
 let days = [
   "Sunday",
   "Monday",
@@ -11,11 +13,19 @@ let days = [
   "Friday",
   "Saturday",
 ];
-console.log(days[now.getDay()]);
-console.log(now.getFullYear());
-let day = now.getDay();
-let hr = now.getHours();
-let min = now.getMinutes();
+console.log(date.getDate());
+console.log(date.getHours());
+let hr = date.getHours();
+if (hr < 10) {
+  hr = `0${hr}`;
+}
+console.log(date.getMinutes());
+let min = date.getMinutes();
+if (min < 10) {
+  min = `0${min}`;
+}
+console.log(days[date.getDay()]);
+console.log(date.getFullYear());
 let time = document.querySelector("#current-time");
 time.innerHTML = `${days[day]} ${hr}:${min} `;
 
@@ -25,27 +35,40 @@ function search(event) {
   console.log(searchInput.value);
   let geo = document.querySelector("#location");
   geo.innerHTML = `${searchInput.value}`;
+  let currentWeather = document.querySelector("#temperature");
+  //let time = document.querySelector("#current-time");
   let description = document.querySelector("#weatherdescriptor");
   let speed = document.querySelector("#wind");
   let humid = document.querySelector("#precipitation");
-  let weathericon = document.querySelector("#icon")
+  let weathericon = document.querySelector("#icon");
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=4a3d357424bef28614271bbf95c59986&&units=metric`;
   axios
     .get(apiUrl)
     .then(function (response) {
       console.log(response);
+      //There is the time of the forecast :D sorry, i mean i cant get it to reflect on my code
+      var forecastTime = new Date(
+        response.data.dt + response.data.timezone * 1000
+      );
+      console.log(forecastTime);
       console.log(response.data.main.temp);
       console.log(response.data.weather[0].description);
       console.log(response.data.wind.speed);
       console.log(response.data.main.humidity);
-    
-      time.innerHTML = `${days[day]} ${hr}:${min} Temperature:${Math.round(
-        response.data.main.temp
-      )}°C`;
+
+      currentWeather.innerHTML = `${Math.round(response.data.main.temp)}C°`;
+      document.getElementById(
+        "current-time"
+      ).innerHTML = `${forecastTime.toLocaleString("en-us", {
+        weekday: "long",
+      })}, ${forecastTime.getHours()}:${forecastTime.getSeconds()}`;
       description.innerHTML = response.data.weather[0].description;
-      speed.innerHTML = `Wind:${response.data.wind.speed} Km/h`;
-      humid.innerHTML = `Humidity:${response.data.main.humidity} %`;
-      weathericon.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`); 
+      speed.innerHTML = `Wind: ${response.data.wind.speed} Km/h`;
+      humid.innerHTML = `Humidity: ${response.data.main.humidity} %`;
+      weathericon.setAttribute(
+        "src",
+        `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+      );
       weathericon.setAttribute("alt", response.data.weather[0].description);
     })
 
